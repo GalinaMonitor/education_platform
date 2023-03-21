8e2c2ff1e23a7668b69812048be690021c997ca7
gmonitor <seregin.da@didenok.com>
2023-03-21 19:30:05 +0300

    feat: add api url in env

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useUserStore from "../store";
 
-export const API_URL='http://localhost:8000/'
+export const API_URL=process.env.REACT_APP_API_URL
 
 const $api = axios.create({
     withCredentials: true,

b769458bce69bafee3af92513f0988b0946ac10a
gmonitor <seregin.da@didenok.com>
2023-03-30 22:20:18 +0300

    fix: microfix

--- frontend/src/services/CourseChapterService.jsx
+++ frontend/src/services/CourseChapterService.jsx
@@ -2,6 +2,6 @@ import $api from "../api";
 
 export default class CourseChapterService {
     static async get_messages(id, limit, page) {
-        return $api.get(`/course_chapter/${id}/messages?size=${limit}&page=${page}`)
+        return $api.get(`/course_chapter/${id}/messages/?size=${limit}&page=${page}`)
     }
 }

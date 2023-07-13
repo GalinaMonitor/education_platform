8c59d72e027e45e11e1bd03306dc713b1964089c
gmonitor <seregin.da@didenokteam.com>
2023-07-13 16:12:02 +0300

    feat: allow html text editing

--- frontend/src/components/messages/TextMessage.jsx
+++ frontend/src/components/messages/TextMessage.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { formatDatetime } from "../../utils/utils";
 
 const TextMessage = ({ text, time = null }) => {
+  const parsedText = <p dangerouslySetInnerHTML={{ __html: text }} />;
   return (
     <div
       className={`border-solid border-2 border-orange-500 rounded-2xl relative p-6 m-10`}
     >
-      <div className={"top-0 left-0 text-left"}>
-        <p>{text}</p>
-      </div>
+      <div className={"top-0 left-0 text-left"}>{parsedText}</div>
       {time ? (
         <div className={"absolute bottom-0 right-2 text-right"}>
           <p>{formatDatetime(time)}</p>

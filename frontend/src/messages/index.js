1fe7b364e369ec420147cc844bac123ced151ef4
gmonitor <seregin.da@didenokteam.com>
2023-10-26 23:14:32 +0300

    feat: change alert message design

--- frontend/src/messages/index.js
+++ frontend/src/messages/index.js
@@ -1,4 +1,24 @@
-import { message } from "antd";
+import { notification } from "antd";
 
-export const success = (text) => message.success(text);
-export const error = (text) => message.error(text);
+export const success = (text) =>
+  notification.success({
+    description: text,
+    duration: 3,
+    placement: "top",
+    closeIcon: null,
+    style: {
+      fontFamily: "Unbounded",
+      backgroundColor: "#e1ffd6",
+    },
+  });
+export const error = (text) =>
+  notification.error({
+    description: text,
+    duration: 3,
+    placement: "top",
+    closeIcon: null,
+    style: {
+      fontFamily: "Unbounded",
+      backgroundColor: "#ffd6d6",
+    },
+  });

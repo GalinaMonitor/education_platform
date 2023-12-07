bd43a7155ef0b05c5f61f4b9d6ea2a1893f46619
gmonitor <seregin.da@didenokteam.com>
2023-12-07 18:13:15 +0300

    feat: add whitespace-pre-line

--- frontend/src/components/messages/TextMessage.jsx
+++ frontend/src/components/messages/TextMessage.jsx
@@ -8,7 +8,9 @@ const TextMessage = ({ text, time = null }) => {
       className={`border-2 border-solid rounded-2xl relative p-6 m-10`}
       style={{ borderColor: "#ff7d1f" }}
     >
-      <div className={"top-0 left-0 text-left title-md"}>{parsedText}</div>
+      <div className={"top-0 left-0 text-left title-md whitespace-pre-line"}>
+        {parsedText}
+      </div>
       {time ? (
         <div className={"absolute bottom-1 right-2 text-right"}>
           <p className={"title-xs"}>{formatDatetime(time)}</p>

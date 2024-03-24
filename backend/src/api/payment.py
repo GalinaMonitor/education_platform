e978c0907e8229cf5e22cc702628582ba0356e3d
gmonitor <seregin.da@didenokteam.com>
2024-03-24 20:04:08 +0300

    feat: fix payment system

--- backend/src/api/payment.py
+++ backend/src/api/payment.py
@@ -30,9 +30,12 @@ async def get_payment_link(
 
 @router.post("/lifepay_callback/")
 async def lifepay_callback(
-    data: LifePayCallbackData, payment_service: PaymentService = Depends(), user_service: UserService = Depends()
+    data: LifePayCallbackData = Form(),
+    payment_service: PaymentService = Depends(),
+    user_service: UserService = Depends(),
 ):
     pprint(data)
+    data = LifePayCallbackData.model_validate(data)
     if data.status != "success":
         return
     user = await user_service.get_by_email(data.email)

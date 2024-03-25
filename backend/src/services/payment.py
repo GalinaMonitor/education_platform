31c3fa03274d999625b0fd0b71ef3274298816e0
Dmitry Seregin <dm.seregin@x5.ru>
2024-03-25 20:52:29 +0300

    fix: change prices

--- backend/src/services/payment.py
+++ backend/src/services/payment.py
@@ -14,12 +14,12 @@ from src.settings import settings
 SUBSCRIPTION_PARAMS = {
     SubscriptionLength.MONTH: {
         "description": "Месячная подписка на Ку-Помогу",
-        "amount": 5,
+        "amount": 5000,
         "duration": timedelta(days=30),
     },
     SubscriptionLength.QUARTER: {
         "description": "Квартальная подписка на Ку-Помогу",
-        "amount": 12,
+        "amount": 12000,
         "duration": timedelta(days=120),
     },
 }

b3efdc6df6a219c5b24e51e19214b273433426c8
gmonitor <seregin.da@didenokteam.com>
2024-06-15 02:30:15 +0300

    feat: fix admin

--- backend/src/admin/config.py
+++ backend/src/admin/config.py
@@ -15,10 +15,58 @@ admin = Admin(
     auth_provider=MyAuthProvider(allow_paths=["/statics/logo.svg"]),
     middlewares=[Middleware(SessionMiddleware, secret_key=settings.secret_key)],
 )
+
+
+class UserView(ModelView):
+    fields = [
+        "is_admin",
+        "is_active",
+        "passed_welcome_page",
+        "time_on_platform",
+        "avatar",
+        "fullname",
+        "company",
+        "job",
+        "email",
+        "end_of_subscription",
+        "subscription_type",
+        "service_uuid",
+    ]
+
+
+class ChatView(ModelView):
+    fields = [
+        "user_id",
+        "receive_time",
+        "is_active",
+        "last_video",
+        "get_welcome_message",
+        "coursechapter_id",
+        "coursechapter",
+        "chat_type",
+    ]
+
+
+class CourseChapterView(ModelView):
+    fields = ["description", "name", "kinescope_project_id", "course_id", "welcome_message", "mentor_id", "mentor"]
+
+
+class ThemeView(ModelView):
+    fields = ["coursechapter_id", "name"]
+
+
+class MessageView(ModelView):
+    fields = ["datetime", "content", "content_type", "is_read", "chat_id", "theme_id", "theme"]
+
+
+class VideoView(ModelView):
+    fields = ["id", "order", "name", "link", "description", "coursechapter_id", "theme_id", "theme"]
+
+
 admin.add_view(ModelView(Course, label="Курс"))
-admin.add_view(ModelView(User, label="Пользователи"))
-admin.add_view(ModelView(Chat, label="Чаты"))
-admin.add_view(ModelView(CourseChapter, label="Уровни курсов"))
-admin.add_view(ModelView(Theme, label="Темы"))
-admin.add_view(ModelView(Message, label="Сообщения"))
-admin.add_view(ModelView(Video, label="Видео"))
+admin.add_view(UserView(User, label="Пользователи"))
+admin.add_view(ChatView(Chat, label="Чаты"))
+admin.add_view(CourseChapterView(CourseChapter, label="Уровни курсов"))
+admin.add_view(ThemeView(Theme, label="Темы"))
+admin.add_view(MessageView(Message, label="Сообщения"))
+admin.add_view(VideoView(Video, label="Видео"))

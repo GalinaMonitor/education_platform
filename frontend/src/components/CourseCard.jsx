735c22c9a308a5a67d9813ed55c2ddeb338bb139
gmonitor <seregin.da@didenokteam.com>
2024-08-08 23:02:45 +0300

    feat: front end ux optimization

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -44,9 +44,13 @@ const CourseCard = ({ course, refresh, selected }) => {
 
   const handleCancelCourseModal = async (courseChapterId = null) => {
     setIsCourseModalOpen(false);
-    setTimeout(refresh, 500);
+    setTimeout(refresh, 1000);
     if (courseChapterId) {
-      setTimeout(navigate, 500, `${RouteNames.COURSE_CHAT}/${courseChapterId}`);
+      setTimeout(
+        navigate,
+        1000,
+        `${RouteNames.COURSE_CHAT}/${courseChapterId}`
+      );
     }
   };
 

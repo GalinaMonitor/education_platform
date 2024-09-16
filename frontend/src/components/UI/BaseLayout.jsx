781ccc1eeea4e66b17de6b798f17635c5c0b07cf
gmonitor <seregin.da@didenokteam.com>
2024-09-16 13:35:17 +0300

    feat: ui fixes

--- frontend/src/components/UI/BaseLayout.jsx
+++ frontend/src/components/UI/BaseLayout.jsx
@@ -7,13 +7,13 @@ const BaseLayout = ({ children }) => {
   return (
     <div className={"h-full"}>
       <div className={"h-1/6 pt-3"}>
-        <Navbar className={"ml-3 mr-3"} />
+        <Navbar className={"ml-16 mr-16"} />
       </div>
       <div className={"h-4/6 pt-3"}>
-        <Row className={"ml-3 mr-3 h-full"}>{children}</Row>
+        <Row className={"ml-16 mr-16 h-full"}>{children}</Row>
       </div>
       <div className={"h-1/6 pt-3"}>
-        <Notifications className={"ml-3 mr-3"} />
+        <Notifications className={"ml-16 mr-16"} />
       </div>
     </div>
   );
